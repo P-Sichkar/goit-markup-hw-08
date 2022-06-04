761b3ac4c3fc560f70f94e17fb1793a35906819a
Pavlo Sichkar <100154434+P-Sichkar@users.noreply.github.com>
2022-06-04 18:08:17 +0300

    18-08

---
+++ js/menu.js
@@ -0,0 +1,14 @@
+(() => {
+  const refs = {
+    openMenuBtn: document.querySelector("[data-menu-open]"),
+    closeMenuBtn: document.querySelector("[data-menu-close]"),
+    menu: document.querySelector("[data-menu]"),
+  };
+
+  refs.openMenulBtn.addEventListener("click", toggleMenu);
+  refs.closeMenulBtn.addEventListener("click", toggleMenu);
+
+  function toggleMenu() {
+    refs.menu.classList.toggle("is-hidden");
+  }
+})();
